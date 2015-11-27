82cba369017c8a23290cb3ad1b7743c5edfd5f4a
Konstantin Zudov <co@zudov.me>
2015-11-27 19:00:50 +0200

    Add GithubLocation

--- src/Purescript/Inspection/ReleaseTag.hs
+++ src/Purescript/Inspection/ReleaseTag.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 module Purescript.Inspection.ReleaseTag
   ( ReleaseTag()
   , getReleaseTags
+  , getGithubLocation
   ) where
 
 import Data.Text (Text)
@@ -14,16 +16,36 @@ import GHC.Generics (Generic())
 import Control.Lens
 import Network.Wreq
 
+import Web.Bower.PackageMeta (PackageName(), runPackageName)
+import Network.URI
+
 -- | Release tags as they appear in Github's releases
 newtype ReleaseTag = ReleaseTag Text deriving (Show, Eq, Ord, Generic, Typeable, Data)
 
-newtype GithubOwner = GithubOwner Text deriving (Show, Eq, Ord, Generic, Typeable, Data)
-newtype GithubRepo  = GithubRepo  Text deriving (Show, Eq, Ord, Generic, Typeable, Data)
+data GithubLocation = GithubLocation GithubOwner GithubRepo
+                    deriving (Show, Eq, Ord, Generic, Typeable, Data)
+newtype GithubOwner = GithubOwner Text
+                    deriving (Show, Eq, Ord, Generic, Typeable, Data)
+newtype GithubRepo = GithubRepo Text
+                   deriving (Show, Eq, Ord, Generic, Typeable, Data)
 
-getReleaseTags :: GithubOwner -> GithubRepo -> IO [ReleaseTag]
-getReleaseTags (GithubOwner owner) (GithubRepo repo) = do
+getReleaseTags :: GithubLocation -> IO [ReleaseTag]
+getReleaseTags (GithubLocation (GithubOwner owner) (GithubRepo repo)) = do
   response <- get url
   pure (ReleaseTag <$> (response ^.. responseBody . values . key "tag_name" . _String))
   where
-    url = "https://api.github.com/repos/" <> Text.unpack owner <> "/"
-                                          <> Text.unpack repo <> "/releases"
+    url = "https://api.github.com/repos/"
+            <> escapeURIString isUnreserved (Text.unpack owner) <> "/"
+            <> escapeURIString isUnreserved (Text.unpack repo) <> "/releases"
+
+getGithubLocation :: PackageName -> IO GithubLocation
+getGithubLocation (runPackageName -> packageName) = do
+  [gitUrl] <- (^.. responseBody . key "url" . _String) <$> get url
+  [_,GithubOwner -> owner, GithubRepo -> repo] <- either fail pure
+    (Text.splitOn "/" <$>
+      (maybe (Left "Must end in .git") Right . Text.stripSuffix ".git"
+         =<< maybe (Left "Cannot parse github url") (Right . Text.pack)
+                   (uriPath <$> parseURI (Text.unpack gitUrl))))
+  pure (GithubLocation owner repo)
+  where
+    url = "https://bower.herokuapp.com/packages/" <> escapeURIString isUnreserved packageName
