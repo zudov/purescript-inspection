30a7e8efb051e1db1029d8fa572ed954a333d0a3
Konstantin Zudov <co@zudov.me>
2015-11-27 17:31:04 +0200

    Added TaskId

--- src/Purescript/Inspection/Task.hs
+++ src/Purescript/Inspection/Task.hs
@@ -1,10 +1,13 @@
 module Purescript.Inspection.Task where
 
 import Data.Typeable (Typeable())
+import Data.Data (Data())
 import GHC.Generics (Generic())
 
 import Purescript.Inspection.BuildConfig
 import Purescript.Inspection.Target
+
+newtype TaskId = TaskId Int deriving (Show, Eq, Ord, Generic, Typeable, Data)
 
 data Task = Task { taskBuildConfig :: BuildConfig
                  , taskTarget      :: Target
